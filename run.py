b0eac0c37b7635484c9be31331534a8f34d5bf78
Martin <m.mrugala@student.mini.pw.edu.pl>
2021-11-13 13:59:59 +0100

    graphics

--- run.py
+++ run.py
@@ -2,43 +2,8 @@ import numpy as np
 
 from common.common_functions import array_to_vector, data_to_array, read_data_as_arrays, read_data_as_vectors
 from hopfield import HopfieldNetwork
-from matplotlib import pyplot as plt
-
-
-def reshape(data):
-    dim = int(np.sqrt(len(data)))
-    data = np.reshape(data, (dim, dim))
-    return data
-
-
-def plot(data, test, predicted, figsize=(5, 6)):
-    data = np.atleast_2d(data)
-    test = np.atleast_2d(test)
-    predicted = np.atleast_2d(predicted)
-    data = [reshape(d) for d in data]
-    test = [reshape(d) for d in test]
-    predicted = [reshape(d) for d in predicted]
-
-    fig, axarr = plt.subplots(len(data), 3, figsize=figsize)
-    for i in range(max(len(data), len(test), len(predicted))):
-        if i == 0:
-            axarr[i, 0].set_title('Train data')
-            axarr[i, 1].set_title("Input data")
-            axarr[i, 2].set_title('Output data')
-        if len(data) > i:
-            axarr[i, 0].imshow(data[i], cmap='gray_r')
-            axarr[i, 0].axis('off')
-        if len(test) > i:
-            axarr[i, 1].imshow(test[i], cmap='gray_r')
-            axarr[i, 1].axis('off')
-        if len(predicted) > i:
-            axarr[i, 2].imshow(predicted[i], cmap='gray_r')
-            axarr[i, 2].axis('off')
-
-    plt.tight_layout()
-    plt.savefig("result.png")
-    plt.show()
-
+from drawing import plot
+from ui.board import *
 
 if __name__ == "__main__":
     data = read_data_as_vectors('./data/small-7x7.csv')
@@ -56,3 +21,6 @@ if __name__ == "__main__":
     print('test_data', data_to_array(test_data, (7, 7)))
     print('res', data_to_array(res, (7, 7)))
     plot(data, test_data, res)
+    board = Board(100, 7)
+    board.overwrite_board(data_to_array(res, (7, 7)))
+    board.run()
