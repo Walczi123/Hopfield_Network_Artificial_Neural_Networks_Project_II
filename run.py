5ee02c74bbb9bfe25d898e16e1b0872829c175cb
Martin <m.mrugala@student.mini.pw.edu.pl>
2021-11-23 23:13:43 +0100

    config part2

--- run.py
+++ run.py
@@ -8,12 +8,16 @@ from ui.board import *
 if __name__ == "__main__":
     STANDARD = True
     USE_ASYNC = False
+    DISTURBED = 100
+    PATHS = ['./data/sets/small-7x7.csv', './data/sets/large-25x25.csv','./data/big_set/ptak1.jpeg']
+    NEURONS = [49, 625, None]
+    IDX=1
+    STANDARD_TO_OMMIT = [] # INDICES OF ELEMENTS THAT SHOULD NOT BE TAKEN INTO ACCOUNT
     if STANDARD:
         seed = 1
-        ommit = [2] # INDICES OF ELEMENTS THAT SHOULD NOT BE TAKEN INTO ACCOUNT
+        ommit = STANDARD_TO_OMMIT
         np.random.seed(seed)
-        data = read_data_as_vectors('./data/sets/small-7x7.csv')
-        # data = read_data_as_vectors('./data/large-25x25.csv')
+        data = read_data_as_vectors(PATHS[IDX])
         ommit.sort(reverse=True)
         for idx in ommit:
             del data[idx]
@@ -23,28 +27,28 @@ if __name__ == "__main__":
         test_data = []
 
         for pattern in train_data:
-            test_data.append(disturb_data(pattern,5))
+            test_data.append(disturb_data(pattern,DISTURBED))
 
         train_data = np.asarray(train_data)
         test_data = np.asarray(test_data)
 
-        nn = HopfieldNetwork(49)
+        nn = HopfieldNetwork(NEURONS[IDX])
         # nn.train_oja(train_data,175, 0.0001)
         nn.train(train_data)
         res = []
         for pattern in test_data:
             res.append(nn.predict(pattern, 10, use_async=USE_ASYNC, async_iter=10))
-        plot(data, test_data, res)
+        plot(train_data, test_data, res)
 #     board = Board(100, 7)
 #     board.overwrite_board(data_to_array(res[0], (7, 7)))
 #     board.run() # UNCOMMENT TO SHOW INTERACTIVE BOARD WITH THE FIRST RESULT
     else:
-        data = read_image_as_vector('./data/big_set/ptak1.jpeg', resize=(80, 60))
+        data = read_image_as_vector(PATHS[IDX], resize=(80, 60))
 
         nn = HopfieldNetwork(len(data))
         nn.train([data])
 
         res = []
-        test = disturb_data(data, 100)
+        test = disturb_data(data, DISTURBED)
         res.append(nn.predict(test, 10))
         plot(data, test, res, size=(60,80))
