7afe2bf214253e52cf76feeac2760c2c193c4ac5
Martin <m.mrugala@student.mini.pw.edu.pl>
2021-11-28 14:32:33 +0100

    oja

--- hopfield.py
+++ hopfield.py
@@ -52,3 +52,38 @@ class HopfieldNetwork():
                 return tmp
             e = e_new
         return tmp
+
+    def train_oja(self, train_data, iter, n):
+        # self.train(train_data)
+
+        # for i in range(iter):
+        #     for x in train_data:
+        #         y = np.matmul(self.weights, x)
+        #         for r in range(self.neuron_num):
+        #             for c in range(self.neuron_num):
+        #                 self.weights[r,c] += n * y[r] * (x[c] - y[r] * self.weights[r][c])
+
+        # self.train(train_data)
+
+        for i in range(iter):
+            Wprev = self.weights.copy()
+            print(f"it: {i}   NORM: {np.linalg.norm(self.weights,2)}")
+            for x in train_data:
+                y = self.weights * x
+                # y = np.sign(y)
+                wd = self.weights * y
+                xd = x - wd
+                d2 = (n*y) * xd
+                self.weights += d2
+                # for i in range(self.neuron_num):
+                #     for j in range(self.neuron_num):
+                #         y2 = y[i]*y[i]
+                #         wy = self.weights[i,j]*y[i]
+                #         xwy = x[j] - self.weights[i,j]*y[i]
+                #         self.weights[i,j] += n*y[i]*(x[j] - self.weights[i,j]*y[i])
+
+
+            if np.linalg.norm(Wprev - self.weights) < 1e-14:
+                break 
+        
+        self.weights -= np.diag(np.diag(self.weights))
